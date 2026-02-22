fca78c55cd0402811acce341d255af18967f1ef0
zhanjinyu <zhanjinyu@bytedance.com>
2026-02-23 00:54:44 +0800

    log: :sparkles: 打卡 2026-02-23

--- docs/.vitepress/userConfig/translations.ts
+++ docs/.vitepress/userConfig/translations.ts
@@ -6,14 +6,14 @@
  * ! 并在该文件中将其名称进行替换。
  */
 export const fileName2Title: Record<string, string> = {
-  AI: '\ud83e\udd16 AI',
-  Java: '\u2615 Java',
-  MQ: '\ud83d\udcec MQ / 消息队列',
-  work: '\ud83d\udcbc 工作记录',
-  基础: '\ud83d\udcd6 计算机基础',
-  架构: '\ud83d\uddfd 架构',
-  站外文档: '\ud83d\udd17 站外文档',
-  简历: '\ud83d\udcc4 简历',
-  算法: '\ud83e\udde0 算法',
-  面经: '\ud83d\udcdd 面经'
+  AI: '🧠 算法 & AI',
+  Java: '☕ Java',
+  MQ: '📬 MQ / 消息队列',
+  work: '💼 工作记录',
+  基础: '📖 计算机基础',
+  架构: '🏗️ 架构',
+  站外文档: '🔗 站外文档',
+  简历: '📄 简历',
+  算法: '📝 刷题记录',
+  面经: '📝 面经'
 };
